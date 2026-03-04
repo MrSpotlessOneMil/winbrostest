1fb2b65d6517fea7c53a1c532ca0c4e93b5faa27
overflow4 <overflow4@users.noreply.github.com>
2026-03-03 17:59:04 -0800

    push

--- components/teams/velocity-fluid-background.tsx
+++ components/teams/velocity-fluid-background.tsx
@@ -628,6 +628,7 @@ export function VelocityFluidBackground({ className }: VelocityFluidBackgroundPr
         width: "100%",
         height: "100%",
         pointerEvents: "none",
+        borderRadius: "inherit",
       }}
     />
   )
